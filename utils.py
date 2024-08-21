5a270e9b99fd3661071ff86582469333c410f13a
Ben Smithers <b.smithers.actual@gmail.com>
2024-08-21 09:16:37 -0700

    only accept pulses in a window near a trigger

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ from picosdk.ps3000a import ps3000a as ps
 from picosdk.functions import adc2mV, assert_pico_ok, mV2adc
 from contextlib import ContextDecorator # used to trigger the compilation
 
-
+from enum import Enum
 import matplotlib.pyplot as plt 
 
 import numpy as np 
@@ -20,6 +20,11 @@ chARange = 5
 
 channelInputRanges = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000, 200000]
 
+class ReturnType(Enum):
+    PulseCount = 0
+    Amplitudes = 1
+    Area = 2
+
 class Scope(ContextDecorator):
     """
         We use the contextdecorator so that this will perform certain actions upon closing
@@ -27,12 +32,18 @@ class Scope(ContextDecorator):
 
     def __enter__(self):
         self._channels={}
+        self._threshs={}
 
         # Displays the staus returns
         self._prepared = False 
         self.chandle = ctypes.c_int16()
         self.status = ps.ps3000aOpenUnit(ctypes.byref(self.chandle ), None)
         self.powerstat =  ps.ps3000aChangePowerSource(self.chandle , 282)
+
+        self._min_offset = 10 # ns? 
+        self._max_offset = 20 
+        self._time_per_sample = -1
+
     def __exit__(self, *exc):
         """
         We stop and close the connection to the picoscope 
@@ -59,6 +70,7 @@ class Scope(ContextDecorator):
         print("Buffering {} segments".format(n_segments))
         status= ps.ps3000aGetTimebase2(self.chandle, 2, MAXSAMPLES, ctypes.byref(timeIntervalns), 1, ctypes.byref(returnedMaxSamples), 0)
         print("Using Time interval: {} ns".format(timeIntervalns))
+        self._time_per_sample = float(timeIntervalns)
 
         assert_pico_ok(status)
         status=ps.ps3000aMemorySegments(self.chandle, n_segments, ctypes.byref(cmaxSamples))
@@ -67,7 +79,7 @@ class Scope(ContextDecorator):
         assert_pico_ok(status)
         self._prepared = True 
 
-    def enable_channel(self, channo, collect=True):
+    def enable_channel(self, channo, collect=True, pulse_threshold=40):
         """
             Enable the channel on the picoscope, and then prepare a Channel object where the buffers will be opened
         """
@@ -77,6 +89,7 @@ class Scope(ContextDecorator):
         self._prepared = False
         if collect: 
             self._channels[channo] = Channel(self, channo)
+            self._threshs[channo] = pulse_threshold
             return self._channels[channo]
 
     def disable_channel(self, channo):
@@ -91,7 +104,7 @@ class Scope(ContextDecorator):
         assert_pico_ok(status)
 
 
-    def sample(self):
+    def sample(self, return_kind=ReturnType.PulseCount):
         if not self._prepared:
             self._prepare()
 
@@ -101,6 +114,7 @@ class Scope(ContextDecorator):
 
         peaks = [0 for _ in self._channels.keys()]
         amps = [[] for _ in self._channels.keys()]
+        times = [[] for _ in self._channels.keys()]
 
         for ic, chankey in enumerate(self._channels.keys()):
             for bx in range(len(self._channels[chankey].bufmin)):
@@ -139,13 +153,36 @@ class Scope(ContextDecorator):
                 parsed = adc2mV(self._channels[chankey].bufmax[i], chARange, maxADC)
                 #plt.plot(range(len(parsed)), sign*np.array(parsed), alpha=0.2)
                 
-                peakfind = find_peaks(sign*np.array(parsed), height=40)
+                # find_peks returns only the index of the peaks, so we need to extrapolate out to get a time
+                # and then also consider that each of these samples are sequential
+                peakfind = find_peaks(sign*np.array(parsed), height=self._threshs[chankey])
+                shifted_times = np.array(peakfind[0])*self._time_per_sample + i*self._time_per_sample*len(parsed)
+
                 peaks[ic] += len(peakfind[0])
+                times[ic] += shifted_times.tolist() 
                 amps[ic] += list(peakfind[1]["peak_heights"])
                 
+        accepted = [[], []]
+        # iterate over the pulse times for channels
+        for pulse_time in range(len(times[1])):
+            # binary search to find the trigger pulses bordering this one
+            index = np.searchsorted(times[0], pulse_time)-1  # minus one to get the proper index of the proceeding pulse 
+            tdiff = pulse_time - times[0][index]
+            accepted[0].append( tdiff>self._min_offset and tdiff<self._max_offset )
+        
+        for pulse_time in range(len(times[[2]])):
+            index = np.searchsorted(times[0], pulse_time)-1  # minus one to get the proper index of the proceeding pulse 
+            tdiff = pulse_time - times[0][index]
+            accepted[1].append( tdiff>self._min_offset and tdiff<self._max_offset )
+        
+        accepted = np.array(accepted)
+
         #return self._channels[chankey].bufmax[i], self._channels[chankey].bufmin[i]
         #plt.show()
-        return peaks 
+        if return_kind==ReturnType.PulseCount:
+            return len(amps[0][accepted]),len(amps[1][accepted])  
+        elif return_kind==ReturnType.Amplitudes:
+            return amps[0][accepted], amps[1][accepted]
 
     def adc2mV(self, bufferADC, maxADC):
         bufferV = bufferADC*channelInputRanges[chARange]/maxADC
